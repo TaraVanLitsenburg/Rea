596d4f8863dc8f48f82fc1de96d9acc931a97f7b
Tara van Litsenburg <tara.vanlitsenburg@edu-kw1c.nl>
2022-02-14 15:28:34 +0100

    Toevoegen instructie les 1

---
+++ Realiseren/Thema3/Hoofdstuk1/Instructie/instructie.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * User: Tara van Litsenburg
+ * Date: 14-02-2021
+ * File: T3_REA_Oefening1.1.php
+ */
+?>
+<!DOCTYPE html>
+<html lang="nl">
+    <head>
+        <title>
+            <?php
+                echo 'Realiseren instructies thema 3 en 4.';
+            ?>
+        </title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="styles/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h1>
+                <?php
+                    echo 'Instructies';
+                ?>
+            </h1>
+        </header>
+        <?php
+            //Hier gaan we in het volgende hoofdstuk het menu toevoegen.
+        ?>
+        <main id="wrapper">
+            <nav>
+                <h2>Hoofdmenu</h2>
+                <h3>Hoofdstuk 1:</h3>
+                <ul>
+                    <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.1.php">Oefening 1.1</a></li>
+                    <li><a href="/REA_T3T4/Realiseren/Thema3/Hoofdstuk1/Oefening/T3_REA_Oefening1.2.php">Oefening 1.2</a></li>
+                </ul>
+            </nav>
+            <section>
+                <h2>
+                    Uitwerkingen.
+                </h2>
+
+                <!-- Plaats je code / uitwerking hieronder
+                <?php
+                    //Een enkele regel commentaar in php
+                    echo "Ik ben Tara";
+                    echo <p>"Mijn achternaam is van Litsenburg"</p>;
+                ?>
+                <p>
+                    <?php
+                        // Voorbeeldcode
+                        echo "Hello World!";
+                    ?>
+                </p>
+            </section>
+        </main>
+    </body>
+</html>
