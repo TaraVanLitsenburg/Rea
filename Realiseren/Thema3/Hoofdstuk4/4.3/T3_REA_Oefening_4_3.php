25ce1d509b5988402fdc97b1198c2587179716c9
Tara van Litsenburg <tara.vanlitsenburg@edu-kw1c.nl>
2022-03-14 16:13:06 +0100

    T3_REA_Oefening_4_3

---
+++ Realiseren/Thema3/Hoofdstuk4/4.3/T3_REA_Oefening_4_3.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Naam: Tara van Litsenburg
+ * Datum: 14-03-2022
+ * File: T3_REA_Oefening_4_3.php
+*/
+    //Taak 1
+    //De header word via een include bij de pagina gevoegd vanuit een andere map
+    include "../../../Thema3/Hoofdstuk3/Oefening/3.2/Include/header.php";
+    //Navigatie
+    include "../../../Thema3/Hoofdstuk3/Oefening/3.2/Include/nav.php";
+    // bestand met de Functions
+    include "../../Hoofdstuk4/4.2/functions.php";
+
+    ?>
