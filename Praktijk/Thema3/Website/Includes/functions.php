b55ce5a68f513939b28daecba0448fadc179ddee
Tara van Litsenburg <tara.vanlitsenburg@edu-kw1c.nl>
2022-03-16 18:13:22 +0100

    Commit

---
+++ Praktijk/Thema3/Website/Includes/functions.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Naam: Tara van Litsenburg
+ * Datum: 15-03-2022
+ * file: functions.php
+*/
+?>
+<link href="/Praktijk/Thema3/Website/Styles/nav.css" rel="stylesheet" type="text/css">
+        <nav>
+            <ul>
+                <li
+                <li><img id = "logo" src="Images/logo.png" alt = "Logo"></li>
+                <li><a href="/Praktijk/Thema3/Website/index.php">Home</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/sport.php">Sport</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/lesrooster.php">Lesrooster</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/personaltrainer.php">Personal Trainer</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/overons.php">Over ons</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/lidworden.php">Lid worden</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/zakelijk.php">Zakelijk</a></li>
+                <li><a href="/Praktijk/Thema3/Website/Pages/contact.php">Contacts</a></li>
+                <li><a href = "/Praktijk/Thema3/Website/Pages/faq.php">FAQ</a></li>
+            </ul>
+        </nav>
