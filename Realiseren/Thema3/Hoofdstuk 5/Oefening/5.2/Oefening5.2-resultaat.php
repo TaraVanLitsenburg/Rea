6c1a1a313e4f88f10e4178317aed0c457ba6a161
Tara van Litsenburg <tara.vanlitsenburg@edu-kw1c.nl>
2022-03-26 17:23:58 +0100

    T3_REA_Oefening_5_2

---
+++ Realiseren/Thema3/Hoofdstuk 5/Oefening/5.2/Oefening5.2-resultaat.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Naam: Tara van Litsenburg
+ * Datum: 26-03-2022
+ * File: Oefening5.2-resultaat.php
+*/
+$username = "Tara van Litsenburg";
+$password = "zelfverzonnen";
+
+if ($username == "Tara van Litsenburg" and $password == "zelfverzonnen")
+{
+    echo "Username and/or Password is incorrect";
+}
+else
+{
+    echo "<img src='../5.2/Images/Schatkist.png'>";
+}
+?>
+<link href="Styles/5.2.css" type="text/css" rel="stylesheet">
---
+++ Realiseren/Thema3/Hoofdstuk 5/Oefening/5.2/Oefening5.2.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * Naam: Tara van Litsenburg
+ * Datum: 26-03-2022
+ * file: Oefening5.2.php
+*/
+?>
+<!Doctype HTML>
+<html lang= "nl">
+    <head>
+        <title>
+            Oefening 5.2
+        </title>
+        <link href="Styles/5.2.css" type="text/css" rel="stylesheet">
+    </head>
+    <body>
+    <form method="post" action="Oefening5.2-resultaat.php">
+            <table>
+                <tr>
+                    <td colspan="2">
+                        <h1>Inloggen</h1>
+                        <p>
+                            Welkom op onze website. <br>
+                            Na correct in te loggen, krijgt u toegang tot onze schatkist. Deze is dan voor u, om te gebruiken!
+                        </p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        Username
+                    </td>
+                    <td>
+                        <input type="text">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        Password
+                    </td>
+                    <td>
+                        <input type="password">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="submit" value="Verzenden">
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
+
+
